24e0026c00a5fd75dc5f7df98cc116365d3f348b
Ammar-Fasih <ammarfasih96@gmail.com>
2022-11-18 18:12:45 +0500

    wave and full hand move integrated/ scoring also has been updated

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import subprocess
 import sys
 from PIL import Image,ImageGrab
 import time
+import math
 
 
 
@@ -24,12 +25,15 @@ video_file_name = "demovideo.mp4"
 camera_to_use = 0
 bounding_box = (0,0,800,800)
 
+
 def main():
 
     HeadModel = True
     focusModel = True
     emotionModel = True
     pointingModel = True
+    waveModel = True
+    handModel = True
 
     
 
@@ -62,7 +66,16 @@ def main():
     filepath2 = 'scorelog/PerSecondData.csv' # csv file for score data of every second.
     filepath3 = 'scorelog/Report.csv' # csv file for videos information and score mapping against videos.
 
-    df = pd.DataFrame(columns=['Date','Time','Focus','Emotion','Head','Hand'])
+    df = pd.DataFrame(columns=['Date','Time','Focus','Emotion','Head','Pointing','Waving','Hand Movement'])
+    
+    def rescaleFrame(frame, scale=1.8):
+        # Images, Videos and Live Video
+        width = int(frame.shape[1] * scale)
+        height = int(frame.shape[0] * scale)
+
+        dimensions = (width,height)
+
+        return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
 
     def calc_landmark_list(image, landmarks,ROI=False,hand = False):
         image_width, image_height = image.shape[1], image.shape[0]
@@ -150,33 +163,51 @@ def main():
         return [x, y, x + w, y + h]
 
 
-    def draw_info_text(image, focus_text,emotion_text='',head_text='',pointing_text =''):
-        cv.rectangle(image, (0, 0), (290,230),
+    def draw_info_text(image, focus_text='',emotion_text='',head_text='',pointing_text ='', wave_text='', move_text=''):
+        cv.rectangle(image, (0, 0), (290,y_coordinate+(7*y_spacing)+5),
                     (0, 0, 0), -1)
 
         if focus_text != "":
             info_text = 'Gaze: ' + focus_text
-            cv.putText(image, info_text, (5,30),
-                    cv.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 1, cv.LINE_AA)
+            cv.putText(image, info_text, (5,y_coordinate),
+                    cv.FONT_HERSHEY_SIMPLEX, font_size, (255, 255, 255), 1, cv.LINE_AA)
         
         if emotion_text != "":
             info_text2 = 'Emotion: ' + emotion_text
-            cv.putText(image, info_text2, (5,70),
-                    cv.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 1, cv.LINE_AA)
+            cv.putText(image, info_text2, (5,y_coordinate+(1*y_spacing)),
+                    cv.FONT_HERSHEY_SIMPLEX, font_size, (255, 255, 255), 1, cv.LINE_AA)
                     
         if head_text != "":
             info_text3 = 'Head: ' + head_text
-            cv.putText(image, info_text3, (5,110),
-                    cv.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 1, cv.LINE_AA)
+            cv.putText(image, info_text3, (5,y_coordinate+(2*y_spacing)),
+                    cv.FONT_HERSHEY_SIMPLEX, font_size, (255, 255, 255), 1, cv.LINE_AA)
 
         if pointing_text != "":
             info_text4 = 'Pointing: ' + pointing_text
-            cv.putText(image, info_text4, (5,150),
-                    cv.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 1, cv.LINE_AA)
+            cv.putText(image, info_text4, (5,y_coordinate+(3*y_spacing)),
+                    cv.FONT_HERSHEY_SIMPLEX, font_size, (255, 255, 255), 1, cv.LINE_AA)
         else:
             info_text4 = 'Pointing: N/A'
-            cv.putText(image, info_text4, (5,150),
-                    cv.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 1, cv.LINE_AA)
+            cv.putText(image, info_text4, (5,y_coordinate+(3*y_spacing)),
+                    cv.FONT_HERSHEY_SIMPLEX, font_size, (255, 255, 255), 1, cv.LINE_AA)
+
+        if wave_text != "":
+            info_text5 = 'Wave: ' + wave_text
+            cv.putText(image, info_text5, (5,y_coordinate+(4*y_spacing)),
+                    cv.FONT_HERSHEY_SIMPLEX, font_size, (255, 255, 255), 1, cv.LINE_AA)
+        else:
+            info_text5 = 'Wave: N/A'
+            cv.putText(image, info_text5, (5,y_coordinate+(4*y_spacing)),
+                    cv.FONT_HERSHEY_SIMPLEX, font_size, (255, 255, 255), 1, cv.LINE_AA)
+
+        if move_text != "":
+            info_text6 = 'Move: ' + move_text
+            cv.putText(image, info_text6, (5,y_coordinate+(5*y_spacing)),
+                    cv.FONT_HERSHEY_SIMPLEX, font_size, (255, 255, 255), 1, cv.LINE_AA)
+        else:
+            info_text6 = 'Wave: N/A'
+            cv.putText(image, info_text6, (5,y_coordinate+(5*y_spacing)),
+                    cv.FONT_HERSHEY_SIMPLEX, font_size, (255, 255, 255), 1, cv.LINE_AA)
 
         val = []
         if focus_text == 'Focused':
@@ -187,10 +218,14 @@ def main():
             val.append(1)
         if pointing_text == 'Pointing':
             val.append(1)
+        if wave_text == 'waving':
+            val.append(1)
+        if move_text == 'moving':
+            val.append(1)
         
 
-        cv.putText(image, f"Score: {sum(val)}/4", (5,190),
-                    cv.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 1, cv.LINE_AA)
+        cv.putText(image, f"Score: {sum(val)}/6", (5,y_coordinate+(6*y_spacing)),
+                    cv.FONT_HERSHEY_SIMPLEX, font_size, (255, 255, 255), 1, cv.LINE_AA)
 
         return image
 
@@ -214,7 +249,8 @@ def main():
             cap.set(cv.CAP_PROP_FRAME_HEIGHT, cap_height)
         
         elif camera == 'video':
-            cap_device = 'videos/video1.mp4'
+            cap_device = 'D:/Internship/Tasks/Training/Moving hand while focus/handwhilefocus (10).mp4'
+            # cap_device = 'D:/Internship/Tasks/Training/Focussed on Screen/1038007019-preview.mp4'
             cap_width = 1920
             cap_height = 1080
             # Camera preparation
@@ -250,7 +286,54 @@ def main():
         cam_rgb.preview.link(xout_rgb.input)
         return pipeline
 
-        
+    def length(a,b):
+        x1 = a[0] 
+        y1 = a[1] 
+        x2 = b[0] 
+        y2 = b[1]
+        return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+    def coordinatesCalc(landmarks, index):
+        point = landmarks.landmark[index]
+        x = int(point.x * image_width)
+        y = int(point.y * image_height)
+        return (x,y)
+
+    def hi5Detection(landmarks, image):
+        indexFingerTip = coordinatesCalc(landmarks, 8)
+        indexFingerPip = coordinatesCalc(landmarks, 6)
+        middleFingerTip = coordinatesCalc(landmarks, 12)
+        middleFingerPip = coordinatesCalc(landmarks, 10)
+        ringFingerTip = coordinatesCalc(landmarks, 16)
+        ringFingerPip = coordinatesCalc(landmarks, 14)
+        pinkyFingerTip = coordinatesCalc(landmarks, 20)
+        pinkyFingerPip = coordinatesCalc(landmarks, 18)
+
+        if indexFingerTip[1] < indexFingerPip [1]:
+            if middleFingerTip[1] < middleFingerPip[1]:
+                if ringFingerTip[1] < ringFingerPip[1]:
+                    if pinkyFingerTip[1] < pinkyFingerPip[1]:
+                        cv.putText(image, 'Hi5', (25,70),
+                        cv.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,0), 1, cv.LINE_AA)
+                        return True
+    
+    # Set the font and spacing of text in black box
+    font_size = 0.7
+    y_coordinate = 30
+    y_spacing = 35
+
+    # Initial frame position of left and right hand (index finger tip)
+    start_waveFrame_left = (0,0)
+    start_waveFrame_right = (0,0)
+
+    # Initial frame position of pose landmarks
+    start_right_wrist = (0,0)
+    start_right_elbow = (0,0)
+    start_right_shoulder = (0,0)
+    start_left_wrist = (0,0)
+    start_left_elbow = (0,0)
+    start_left_shoulder = (0,0)
+
     mode = 0
 
     # Face Model load
@@ -265,6 +348,12 @@ def main():
     mpHands = mp.solutions.hands
     hands = mpHands.Hands(max_num_hands=2, min_detection_confidence=0.7)
     mpDraw = mp.solutions.drawing_utils
+
+    # Pose Model Load
+    mp_pose = mp.solutions.pose
+    pose = mp_pose.Pose(
+        min_detection_confidence=0.7,
+        min_tracking_confidence=0.8)
 
 
     # Initializing keypoint classifiers and labels list
@@ -315,11 +404,10 @@ def main():
         prev_frame_time = new_frame_time
     
         # converting the fps into integer
-        fps = int(fps)
+        fps = str(int(fps))
     
         # converting the fps to string so that we can display it on frame
         # by using putText function
-        fps = str(fps)
 
 
         # print('Next1',next)
@@ -337,6 +425,11 @@ def main():
             image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
         elif camera == 'cam' or camera == 'video':
             ret, image = cap.read()
+            try:
+                if camera == 'video':
+                    image = rescaleFrame(image, scale=2)
+            except:
+                pass
             if not ret:
                 break
         
@@ -352,6 +445,7 @@ def main():
         image.flags.writeable = False
         face_results = face_mesh.process(image)
         hand_results = hands.process(image)
+        pose_results = pose.process(image)
         image.flags.writeable = True
 
         if face_results.multi_face_landmarks is not None:
@@ -366,6 +460,9 @@ def main():
                     pre_processed_landmark_list1 = pre_process_landmark(
                     focus_landmark_list)
                     facial_focus_id = focus_keypoint_classifier(pre_processed_landmark_list1)
+                    eyeFocusVal = focus_labels[facial_focus_id]
+                else:
+                    eyeFocusVal = 'N/A'
 
                 if emotionModel == True:
                     emotion_landmark_list = calc_landmark_list(debug_image, face_landmarks)
@@ -373,6 +470,9 @@ def main():
                     pre_processed_landmark_list2 = pre_process_landmark(
                     emotion_landmark_list)
                     facial_emotion_id = emotion_keypoint_classifier(pre_processed_landmark_list2)
+                    emotionVal = emotion_labels[facial_emotion_id]
+                else:
+                    emotionVal = 'N/A'
 
                 if HeadModel == True:
                     head_landmark_list = calc_landmark_list(debug_image, face_landmarks,ROI2)
@@ -380,25 +480,26 @@ def main():
                     pre_processed_landmark_list3 = pre_process_landmark(
                     head_landmark_list)
                     head_id = head_keypoint_classifier(pre_processed_landmark_list3)
+                    headVal = head_labels[head_id]
+                else:
+                    headVal = 'N/A'
 
                 debug_image = draw_bounding_rect(use_brect, debug_image, brect)
                 debug_image = draw_info_text(
-                                debug_image,
-                                focus_labels[facial_focus_id],emotion_labels[facial_emotion_id],head_labels[head_id])
+                                debug_image,eyeFocusVal,emotionVal,headVal)
                 # image_width2, image_height2
                 cv.rectangle(debug_image, (290, 0), (tab,30),(0,255,0), -1)
                 cv.putText(debug_image, "FACE DETECTED", (310,23),cv.FONT_HERSHEY_SIMPLEX, 0.6, (50, 50, 50), 1, cv.LINE_AA)
         else:
-            eyeFocusVal = 'Not Focused'
-            emotionVal = 'Negative'
-            headVal = 'Not Center'
-            df = score(eyeFocusVal,emotionVal,headVal,next,df)
+            eyeFocusVal = 'N/A'
+            emotionVal = 'N/A'
+            headVal = 'N/A'
             cv.rectangle(debug_image, (290, 0), (tab,30),(0,0,255), -1)
             cv.putText(debug_image, "CANNOT DETECT FACE", (310,23),
                     cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1, cv.LINE_AA)
 
         if hand_results.multi_hand_landmarks is not None:
-            for hand_landmarks in hand_results.multi_hand_landmarks:
+            for hand_no, hand_landmarks in enumerate(hand_results.multi_hand_landmarks):
                 if pointingModel == True:
                     mpDraw.draw_landmarks(debug_image, hand_landmarks, mpHands.HAND_CONNECTIONS)
                     pointing_landmark_list = calc_landmark_list(debug_image, hand_landmarks)
@@ -406,37 +507,103 @@ def main():
                     pre_processed_landmark_list4 = pre_process_landmark(
                     pointing_landmark_list)
                     pointing_id = pointing_keypoint_classifier(pre_processed_landmark_list4)
+                    pointingVal = pointing_labels[pointing_id]
                     debug_image = draw_info_text(
-                                debug_image,
-                                focus_labels[facial_focus_id],emotion_labels[facial_emotion_id],head_labels[head_id],pointing_labels[pointing_id]
-                                )
-                    cv.rectangle(debug_image, (tab+1, 0), (tab2,30),(0,255,0), -1)
-                    cv.putText(debug_image, "HANDS DETECTED", (tab+20,23),
+                                debug_image,eyeFocusVal,emotionVal,headVal,pointingVal)
+
+                else:
+                    pointingVal = 'N/A'
+
+                if waveModel == True:
+                    image_width, image_height = image.shape[1], image.shape[0]
+
+                    if hand_no == 0:
+                        next_frame_left = coordinatesCalc(hand_landmarks,8)
+                        difference = length(start_waveFrame_left,next_frame_left)
+                        
+                        if difference > 30 and hi5Detection(hand_landmarks, image):
+                            waveVal_0 = 'waving'
+                        else:
+                            waveVal_0 = 'not waving'
+                        start_waveFrame_left = next_frame_left
+                    if hand_no == 1:
+                        next_frame_right = coordinatesCalc(hand_landmarks,8)
+                        difference = length(start_waveFrame_right,next_frame_right)
+                        if difference > 30 and hi5Detection(hand_landmarks, image):
+                            waveVal_1 = 'waving'
+                        else:
+                            waveVal_1 = 'not waving'
+                        start_waveFrame_right = next_frame_right
+                    else:
+                        waveVal_1 = 'not waving'
+
+                    if waveVal_0 == 'waving' or waveVal_1 == 'waving':
+                        waveVal = 'waving'
+                    else:
+                        waveVal = 'not waving'
+                    debug_image = draw_info_text(
+                            debug_image,eyeFocusVal,emotionVal,headVal,pointingVal,waveVal)
+
+                else:
+                    waveVal = 'N/A'
+
+                cv.rectangle(debug_image, (tab+1, 0), (tab2,30),(0,255,0), -1)
+                cv.putText(debug_image, "HANDS DETECTED", (tab+20,23),
                     cv.FONT_HERSHEY_SIMPLEX, 0.6, (50, 50, 50), 1, cv.LINE_AA)
+
         else:
-            eyeFocusVal = 'Not Focused'
-            emotionVal = 'Negative'
-            headVal = 'Not Center'
-            df = score(eyeFocusVal,emotionVal,headVal,next,df)
+            pointingVal = 'N/A'
+            waveVal = 'N/A'
             cv.rectangle(debug_image, (tab+1, 0), (int(tab2),30),
                     (0, 0,255), -1)
             cv.putText(debug_image, "CANNOT DETECT HANDS", (tab+20,23),
-                    cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1, cv.LINE_AA)
+                    cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1, cv.LINE_AA)    
 
+        if pose_results.pose_landmarks is not None:
+            if handModel == True:
+                image_width, image_height = image.shape[1], image.shape[0]
+
+                # Calculating next coordinates for pose landmarks
+                next_right_wrist = coordinatesCalc(pose_results.pose_landmarks,15)
+                next_right_elbow = coordinatesCalc(pose_results.pose_landmarks,13)
+                next_right_shoulder = coordinatesCalc(pose_results.pose_landmarks,11)
+                next_left_wrist = coordinatesCalc(pose_results.pose_landmarks,16)
+                next_left_elbow = coordinatesCalc(pose_results.pose_landmarks,14)
+                next_left_shoulder = coordinatesCalc(pose_results.pose_landmarks,12)
+
+                # Calculating difference of initial and next frame
+                diff_right_wrist = length(start_right_wrist, next_right_wrist)
+                diff_left_wrist = length(start_left_wrist, next_left_wrist)
+                diff_right_elbow = length(start_right_elbow,next_right_elbow)
+                diff_left_elbow = length(start_left_elbow, next_left_elbow)
+
+                # Checking condition of movement
+                if diff_right_wrist > 17:
+                    moveVal_0 = 'moving'
+                else:
+                    moveVal_0 = 'not moving'
+                start_right_wrist = next_right_wrist
+
+                if diff_left_wrist > 17:
+                    moveVal_1 = 'moving'
+                else:
+                    moveVal_1 = 'not moving'
+                start_left_wrist = next_left_wrist
+            
+                if moveVal_0 == 'moving' or moveVal_1 == 'moving':
+                    moveVal = 'moving'
+                else:
+                    moveVal = 'not moving'
+                debug_image = draw_info_text(
+                            debug_image,eyeFocusVal,emotionVal,headVal,pointingVal,waveVal,moveVal)
+            else:
+                moveVal = 'N/A'
                 
-
         # Drawing part
         try:
-            # debug_image = draw_info_text(
-            #                     debug_image,
-            #                     focus_labels[facial_focus_id],emotion_labels[facial_emotion_id],head_labels[head_id],pointing_labels[pointing_id]
-            #                     )
             # Scoring part
-            eyeFocusVal = focus_labels[facial_focus_id]
-            emotionVal = emotion_labels[facial_emotion_id]
-            headVal = head_labels[head_id]
-            df = score(eyeFocusVal,emotionVal,headVal,next,df)
-            cv.putText(debug_image, f"FPS: {fps}", (5,220),cv.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 1, cv.LINE_AA)
+            cv.putText(debug_image, f"FPS: {fps}", (5,y_coordinate+(7*y_spacing)),cv.FONT_HERSHEY_SIMPLEX, font_size, (255, 255, 255), 1, cv.LINE_AA)
+            df = score(eyeFocusVal,emotionVal,headVal,pointingVal,waveVal,moveVal,next,df)
         except Exception as e:
             print("the error is :", e)
             
--- scoring.py
+++ scoring.py
@@ -4,7 +4,7 @@ import numpy as np
 
 timelist = []
 
-def score(s1,s2,s3,timeVal,df):
+def score(s1,s2,s3,s4,s5,s6,timeVal,df):
     # Calculating score based on Focus, Emotion and Head Values
     if str(s1).lower() == "focused":
         focus = 1
@@ -18,17 +18,29 @@ def score(s1,s2,s3,timeVal,df):
         head = 1
     else:
         head = 0
+    if str(s4).lower() == "pointing":
+        hand = 1
+    else:
+        hand = 0
+    if str(s5).lower() == "waving":
+        waving = 1
+    else:
+        waving = 0
+    if str(s6).lower() == "moving":
+        moving = 1
+    else:
+        moving = 0
 
     date = str(timeVal.strftime("%Y-%m-%d"))
     time = str(timeVal.strftime("%H:%M:%S:%f")[:-3])
 
-    df_new_row = pd.DataFrame({'Date':[date],'Time':[time],'Focus':[focus],'Emotion':[emotion],'Head':[head]})
+    df_new_row = pd.DataFrame({'Date':[date],'Time':[time],'Focus':[focus],'Emotion':[emotion],'Head':[head],'Pointing':[hand],'Waving':[waving],'Hand Movement':[moving]})
     df = pd.concat([df, df_new_row])
     
     return df
 
 def secondScore(df):
-    df2 = pd.DataFrame(columns=['Date','Time','Focus','Emotion','Head','Total'])
+    df2 = pd.DataFrame(columns=['Date','Time','Focus','Emotion','Head','Pointing','Waving','Hand Movement','Total'])
     initial = 0
     startTime = datetime.datetime.strptime(df['Time'].iloc[0], "%H:%M:%S:%f")
     for i in range(len(df)):
@@ -39,13 +51,19 @@ def secondScore(df):
             x = (df.iloc[initial:(i+1),2]).tolist() #Slicing Focus values for 1 second and converting to list
             y = (df.iloc[initial:(i+1),3]).tolist() #Slicing Emotion values for 1 second and converting to list
             z = (df.iloc[initial:(i+1),4]).tolist() #Slicing Head values for 1 second and converting to list
+            w = (df.iloc[initial:(i+1),5]).tolist() #Slicing Head values for 1 second and converting to list
+            u = (df.iloc[initial:(i+1),6]).tolist() #Slicing Head values for 1 second and converting to list
+            v = (df.iloc[initial:(i+1),7]).tolist() #Slicing Head values for 1 second and converting to list
             initial = i
             x2  = max(x,key = x.count) #getting the most frequent score value for Focus
             y2  = max(y,key = y.count) #getting the most frequent score value for Emotion
             z2  = max(z,key = z.count) #getting the most frequent score value for Head
-            total = x2+y2+z2 #Total score 
+            w2  = max(w,key = w.count) #getting the most frequent score value for Head
+            u2  = max(u,key = u.count) #getting the most frequent score value for Head
+            v2  = max(v,key = v.count) #getting the most frequent score value for Head
+            total = x2+y2+z2+w2+u2+v2 #Total score 
 
-            df2_new_row = pd.DataFrame({'Date':[df['Date'].iloc[i]],'Time':[df['Time'].iloc[i]],'Focus':[x2],'Emotion':[y2],'Head':[z2],'Total':[total]})    
+            df2_new_row = pd.DataFrame({'Date':[df['Date'].iloc[i]],'Time':[df['Time'].iloc[i]],'Focus':[x2],'Emotion':[y2],'Head':[z2],'Pointing':[w2],'Waving':[u2],'Hand Movement':[v2],'Total':[total]})    
             df2 = pd.concat([df2, df2_new_row],ignore_index=True) #Adding the values to the dataframe
     return df2
 
@@ -61,20 +79,26 @@ def videoMapping(inputdf):
     Focus={}
     Emotion={}
     Head={}
+    Pointing={}
+    Waving={}
+    Moving={}
     Total={}
     for i in range(lendf):
         Focus[f'video{i+1}'] = sum((df3.iloc[i*y:(i+1)*y,2]).tolist())  
         Emotion[f'video{i+1}'] = sum((df3.iloc[i*y:(i+1)*y,3]).tolist())  
         Head[f'video{i+1}'] = sum((df3.iloc[i*y:(i+1)*y,4]).tolist())  
-        Total[f'video{i+1}'] = sum((df3.iloc[i*y:(i+1)*y,5]).tolist())  
-    # Emotion = {'video1':sum((df3.iloc[0:y,3]).tolist()),'video2':sum((df3.iloc[y:(2*y),3]).tolist()),'video3':sum((df3.iloc[(2*y):(3*y),3]).tolist())}  
-    # Head = {'video1':sum((df3.iloc[0:y,4]).tolist()),'video2':sum((df3.iloc[y:(2*y),4]).tolist()),'video3':sum((df3.iloc[(2*y):(3*y),4]).tolist())}  
-    # Total = {'video1':sum((df3.iloc[0:y,5]).tolist()),'video2':sum((df3.iloc[y:(2*y),5]).tolist()),'video3':sum((df3.iloc[(2*y):(3*y),5]).tolist())}  
-
+        Pointing[f'video{i+1}'] = sum((df3.iloc[i*y:(i+1)*y,5]).tolist())  
+        Waving[f'video{i+1}'] = sum((df3.iloc[i*y:(i+1)*y,6]).tolist())  
+        Moving[f'video{i+1}'] = sum((df3.iloc[i*y:(i+1)*y,7]).tolist())  
+        Total[f'video{i+1}'] = sum((df3.iloc[i*y:(i+1)*y,8]).tolist())  
+    
     
     outputdf['Focus'] = Focus.values()
     outputdf['Emotion'] = Emotion.values()
     outputdf['Head'] = Head.values()
+    outputdf['Pointing'] = Pointing.values()
+    outputdf['Waving'] = Waving.values()
+    outputdf['Hand Movement'] = Moving.values()
     outputdf['Total Score'] = Total.values()
     
     return outputdf
